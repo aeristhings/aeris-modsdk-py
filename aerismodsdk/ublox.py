3fb6e49b3874133c65f61265258627c74427a3e9
sundararunachal <sundar.arunachalam@aeris.net>
2020-02-14 03:43:04 +0000

    Adding missing init method

--- aerismodsdk/ublox.py
+++ aerismodsdk/ublox.py
@@ -6,6 +6,10 @@ Host: www.aeris.com
 
 """
 
+def init(modem_port_config):
+    modem_port = '/dev/tty' + modem_port_config
+    rmutils.init(modem_port)
+
 
 def check_modem():
     ser = rmutils.init_modem()
